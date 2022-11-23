c1042b2d80789064e3d08a12e440dd6e724d962a
Rebecca Murray <117570907+RebMDesign@users.noreply.github.com>
2022-11-23 22:57:40 +0000

    adds hamburger menu
    adds hamburger menu for mobile screen pages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Hi! Welcome to my Portfolio website");
+function menuToggle() {
+    var x = document.getElementById('mynavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
