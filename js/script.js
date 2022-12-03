e6ddf3c9ac2b0ec84307e611f2a0b706402cfd0e
Rebecca Murray <117570907+RebMDesign@users.noreply.github.com>
2022-12-03 21:59:22 +0000

    resolves linter warnings
    resolves linter warnings and formats to prettier.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 var isOpen = false;
 
 function menuToggle() {
-    isOpen = !isOpen;
+  isOpen = !isOpen;
 
-    var x = document.getElementById('mynavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
-    var y = document.getElementById('header');
-    if (y.className === '') {
-        y.className += 'responsive';
-    } else {
-        y.className = '';
-    }
-    var icon = document.querySelector('.icon > i');
-    if (icon.className === 'fa-solid fa-bars') {
-        icon.className = 'fa-solid fa-x';
-    } else {
-        icon.className = 'fa-solid fa-bars';
-    }
+  var x = document.getElementById("mynavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+  var y = document.getElementById("header");
+  if (y.className === "") {
+    y.className += "responsive";
+  } else {
+    y.className = "";
+  }
+  var icon = document.querySelector(".icon > i");
+  if (icon.className === "fa-solid fa-bars") {
+    icon.className = "fa-solid fa-x";
+  } else {
+    icon.className = "fa-solid fa-bars";
+  }
 
-    if (isOpen) {
-        $("#mynavtoggle").hide().fadeIn({ duration: 1000 });
-    }
+  if (isOpen) {
+    $("#mynavtoggle").hide().fadeIn({ duration: 1000 });
+  }
 }
 
 addEventListener("resize", (event) => {
-    if (window.innerWidth > 640 && isOpen) {
-        menuToggle();
-    }
+  if (window.innerWidth > 640 && isOpen) {
+    menuToggle();
+  }
 });
